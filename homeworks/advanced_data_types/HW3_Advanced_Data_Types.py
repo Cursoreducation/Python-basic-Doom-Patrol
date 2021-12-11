220a8b0be21136992573d7e450f63fcf7a91cd56
Andrii Mazurok <andriy.mazurok.tl@gmail.com>
2021-12-11 16:36:46 +0200

    Test1

--- homeworks/advanced_data_types/HW3_Advanced_Data_Types.py
+++ homeworks/advanced_data_types/HW3_Advanced_Data_Types.py
@@ -1,2 +1,42 @@
-print('Test')
-print(2 + 3)
+# 1. Define the id of next variables:
+# int_a = 55
+# str_b = 'cursor'
+# set_c = {1, 2, 3}
+# lst_d = [1, 2, 3]
+# dict_e = {'a': 1, 'b': 2, 'c': 3}
+
+int_a = 55
+str_b = 'cursor'
+set_c = {1, 2, 3}
+lst_d = [1, 2, 3]
+dict_e = {'a': 1, 'b': 2, 'c': 3}
+
+print(id(int_a))
+print(id(str_b))
+print(id(set_c))
+print(id(lst_d))
+print(id(dict_e))
+
+# 2. Append 4 and 5 to the lst_d and define the id one more time.
+
+lst_d.append(4)
+lst_d.append(5)
+
+print(id(lst_d))
+# 3. Define the type of each object from step 1.
+
+print(type(int_a))
+print(type(str_b))
+print(type(set_c))
+print(type(lst_d))
+print(type(dict_e))
+
+# 4*. Check the type of the objects by using isinstance.
+
+print(isinstance(int_a, int))
+print(isinstance(str_b, int))
+print(isinstance(set_c, set))
+print(isinstance(lst_d, set))
+print(isinstance(dict_e, dict))
+
+
